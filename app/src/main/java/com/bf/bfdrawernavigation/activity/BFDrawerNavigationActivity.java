f03e418f3d4b72af65068ddd737890438dc408c8
BFAlex <838318741@qq.com>
2017-12-08 15:35:49 +0800

    增加了Drawer滑动触发器

--- app/src/main/java/com/bf/bfdrawernavigation/activity/BFDrawerNavigationActivity.java
+++ app/src/main/java/com/bf/bfdrawernavigation/activity/BFDrawerNavigationActivity.java
@@ -1,11 +1,15 @@
 package com.bf.bfdrawernavigation.activity;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.bf.bfdrawernavigation.R;
@@ -18,10 +22,10 @@ public class BFDrawerNavigationActivity extends Activity {
 
     public static final String BFTag = "BFTAG";
 
+    private ActionBarDrawerToggle drawerToggle;
+
     /* View */
-    private DrawerLayout mainLayout;
-//    private Fragment mainFragment;
-//    private Fragment drawerView;
+    private DrawerLayout drawerLayout;
     private ListView leftMenu;
 
     /* Data */
@@ -30,7 +34,6 @@ public class BFDrawerNavigationActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_drawer_navigation);
         this.findViews();
         this.setupDefaultData();
@@ -39,7 +42,7 @@ public class BFDrawerNavigationActivity extends Activity {
 
     /* Private */
     private void findViews() {
-        mainLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         leftMenu = (ListView)findViewById(R.id.left_menu);
     }
 
@@ -53,6 +56,43 @@ public class BFDrawerNavigationActivity extends Activity {
     }
 
     private void setupLeftMenu() {
+        /*设置菜单导航栏数据适配器*/
 //        this.leftMenu.setAdapter(new ArrayAdapter<String>(this, null, leftMenuDataSource));
+        /*添加点击事件*/
+        leftMenu.setOnItemClickListener(new DrawerItemClickListener());
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+                null,
+                R.string.drawer_open,
+                R.string.drawer_close) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                Log.d(BFTag, "onDrawerClosed");
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                Log.d(BFTag, "onDrawerOpened");
+            }
+        };
+        drawerLayout.addDrawerListener(drawerToggle);
+    }
+
+
+    private void selectItem(int index) {
+        Log.d(BFTag, "点击item对应的索引号:" + index);
+    }
+
+    /*内部类*/
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            selectItem(i);
+        }
     }
 }
+
